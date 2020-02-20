0df91c9be6410dc77f6054d8b4f7e50aeb775567
EC2 Default User <ec2-user@ip-172-31-39-19.us-east-2.compute.internal>
2020-02-20 03:27:56 +0000

    add changes

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,14 @@ FROM python:3.7.3-stretch
 WORKDIR /app
 
 # Copy source code to working directory
-COPY . app.py /app/
+COPY . app.py /app/ 
+COPY . cleaned.csv /app/
 
 # Install packages from requirements.txt
 # hadolint ignore=DL3013
 RUN pip install --upgrade pip &&\
     pip install --trusted-host pypi.python.org -r requirements.txt
+
+ENTRYPOINT ["python"]
+
+CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-#!/usr/bin/env python
-import click
+from flask import Flask, request #, flash, render_template, request, url_for, redirect
+import pandas as pd
+app = Flask(__name__)
 
-@click.command()
-def hello():
-    click.echo('Hello World!')
+url = 'https://raw.githubusercontent.com/jtwang1027/contint/master/cleaned.csv'
+data = pd.read_csv(url,index_col=0)
+
+
+@app.route('/', methods=['GET', 'POST']) #get/post generates a request object
+def search():
+    #post: page generated after data is submitted;
+    #you can use .form.get method to variables submitted
+    #get: what u see when u access URL directly by typing
+    if request.method=="POST":
+        symp= request.form.get('symptom') #based on name=
+        symp=symp.lower() #lower case
+        diag= data.diagnose[data.symptom.str.contains(symp)].unique()
+
+        if len(diag)==0: #no matches found
+            return '<h1>Matches not found. Please try again. </h1>'
+        else:
+           # out= ','.join(diag)
+            out= '<br/>'.join(diag)
+            return '<h1>The possible diagnoses are:</h1> {}'.format( out)
+
+
+    return '''<form method="POST">
+                  Symptom: <input type="text" name="symptom"><br>
+        
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+
+
 
 if __name__ == '__main__':
-    hello()
+    app.run(host='0.0.0.0', port=5000, debug=True)
